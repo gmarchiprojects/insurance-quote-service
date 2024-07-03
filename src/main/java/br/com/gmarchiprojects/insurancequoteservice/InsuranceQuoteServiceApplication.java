fc11c2b971fcb0ec9aa4cf592175641ab1a02e7b
Giovanni Marchi <giovanni.marchi@compasso.com.br>
2024-07-03 10:46:30 -0300

    feat:basic structure

---
+++ src/main/java/br/com/gmarchiprojects/insurancequoteservice/InsuranceQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmarchiprojects.insurancequoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceQuoteServiceApplication.class, args);
+	}
+
+}
